9b4af9f0438d1810cbb937cc28f13f65876e9fa3
daredevilq <110993908+daredevilq@users.noreply.github.com>
2023-12-10 17:32:39 +0100

    GridPane added

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -8,6 +8,9 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
@@ -16,6 +19,7 @@ import javafx.scene.layout.RowConstraints;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.Map;
 
 public class SimulationPresenter implements MapChangeListener {
 
@@ -35,10 +39,57 @@ public class SimulationPresenter implements MapChangeListener {
         this.map = map;
 
     }
-
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().get(0)); // hack to retain visible grid lines
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
+    }
 
     public void drawMap(WorldMap map){
-        infoLabel.setText(map.toString());
+        clearGrid();
+
+        Boundary boundaries =  map.getCurrentBoundary();
+        for (int i = boundaries.leftBottom().getX(); i <= boundaries.rightTop().getX() + 1; i++) {
+            mapGrid.getColumnConstraints().add( new ColumnConstraints(CELL_WIDTH));
+        }
+
+        for (int i = boundaries.leftBottom().getY(); i <= boundaries.rightTop().getY() + 1; i++) {
+            mapGrid.getRowConstraints().add( new RowConstraints(CELL_HEIGHT));
+        }
+
+        mapGrid.add(new Label("y/x "), 0, 0);
+
+        for (int i = boundaries.leftBottom().getX(); i < boundaries.rightTop().getX() + 1; i++) {
+            Label label = new Label(Integer.toString(i));
+            mapGrid.add(label, i + 1 - boundaries.leftBottom().getX() , 0);
+            GridPane.setHalignment(label, HPos.CENTER);
+            GridPane.setValignment(label, VPos.CENTER);
+        }
+
+        for (int i = boundaries.leftBottom().getY(); i < boundaries.rightTop().getY() + 1; i++) {
+            Label label = new Label(Integer.toString(i));
+
+            GridPane.setHalignment(label, HPos.CENTER);
+            GridPane.setValignment(label, VPos.CENTER);
+
+            mapGrid.add(new Label(Integer.toString(i)),0, i + 1 - boundaries.leftBottom().getY());
+
+        }
+
+        Map<Vector2d, WorldElement> mapElements = map.getElements();
+
+        for (Map.Entry<Vector2d, WorldElement> entry : mapElements.entrySet()) {
+            Vector2d key = entry.getKey();
+            WorldElement value = entry.getValue();
+            Label label = new Label(value.toString());
+
+            GridPane.setHalignment(label, HPos.CENTER);
+            GridPane.setValignment(label, VPos.CENTER);
+
+            mapGrid.add(label, key.getX() + 1 - boundaries.leftBottom().getX(),  key.getY() + 1 - boundaries.leftBottom().getY());
+
+        }
+
 
     }
 
@@ -51,10 +102,9 @@ public class SimulationPresenter implements MapChangeListener {
         });
 
     }
+
     private boolean isSimulationRunning = false;
     public void onSimulationStartClicked(ActionEvent actionEvent) {
-
-
 
         if (!isSimulationRunning) {
             isSimulationRunning = true;
@@ -78,5 +128,5 @@ public class SimulationPresenter implements MapChangeListener {
         }
     }
 
-    }
+}
 
